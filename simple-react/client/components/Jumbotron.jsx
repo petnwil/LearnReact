396836630032f7372a28b7bde6919aad7912b9f6
petnwil <peter.n.wilhelmsen@gmail.com>
2016-07-14 20:03:49 +0200

    Added two chapters

--- simple-react/client/components/Jumbotron.jsx
+++ simple-react/client/components/Jumbotron.jsx
@@ -3,6 +3,11 @@ Jumbotron = React.createClass({
     return(
       <div className="jumbotron">
         <h3> New text in jumbotron <h3>
+
+        <p>
+        This is a new chapter for a the newTest-branch
+        </p>
+        <p> Another paragraph </p>
       </div>
     )
   }
